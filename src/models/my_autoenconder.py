bbe6b2743f99316dc621980536a00b744b5d24b1
bruAristimunha <a.bruno@aluno.ufabc.edu.br>
2020-02-07 20:35:20 -0300

    Adding my_auto_enconder, style sklearn... stage: draft

---
+++ src/models/my_autoenconder.py
@@ -0,0 +1,89 @@
+import tensorflow as tf
+from tensorflow.keras.layers import Input, Dense, Flatten
+from tensorflow.keras.models import Model
+from tensorflow.keras import losses
+from sklearn.model_selection import train_test_split
+
+class my_autoenconder(object):
+
+    def __init__(self, epochs=10, batch_size=32, validation_size=0.2,
+                 random_state=42, regularizer = [],
+                 exposure = None, modality = None):
+
+        # auto-enconder parameters
+        self.value_encoding_dim = []
+        self.regularizer = regularizer
+        self.batch_size = batch_size
+        self.epochs = epochs
+        
+        # validation split parameters
+        self.test_size = validation_size
+        self.random_state = random_state
+        
+        # information about when is use
+        self.exposure = exposure
+        self.modality = modality
+
+        # information about history train
+        self.history = []
+
+        # saving method
+        self.method_autoenconder = []
+        self.method_enconder = []
+
+    def make_auto_enconder(self, value_encoding_dim):
+
+        self.value_encoding_dim = value_encoding_dim
+        
+        fun_loss = losses.mean_absolute_error
+
+        original_trial = Input(shape=(62))
+        enconded = Dense(62, activation='relu')(original_trial)
+        enconded = Dense(32, activation='relu')(enconded)
+        enconded = Dense(16, activation='relu')(enconded)
+
+        if (self.regularizer != []):
+            enconded = Dense(self.value_encoding_dim,
+                             activation='relu',
+                             activity_regularizer=self.regularizer
+                             )(enconded)
+        else:
+            enconded = Dense(self.value_encoding_dim,
+                             activation='relu')(enconded)
+
+        deconded = Dense(16, activation='relu', use_bias=False)(enconded)
+        deconded = Dense(32, activation='relu', use_bias=False)(deconded)
+        deconded = Dense(62, activation='relu', use_bias=False)(deconded)
+
+        encoder = Model(original_trial, enconded, name='encoder')
+        autoencoder = Model(original_trial, deconded, name='autoenconder')
+
+        autoencoder.compile(optimizer='adam', loss=fun_loss,
+                            metrics=['accuracy'])
+
+        self.method_autoenconder = autoencoder
+        self.method_enconder = encoder
+
+    def fit(self, X):
+
+ 
+        # Splitting for validation
+        X_train, X_validation = train_test_split(X.T,
+            test_size=self.test_size,
+            random_state=self.random_state)
+
+        #import pdb; pdb.set_trace()
+        X_train = X_train.to_numpy()
+        X_validation = X_validation.to_numpy()
+
+        # Training auto-enconder
+        self.history = self.method_autoenconder.fit(X_train, X_train,
+                                       epochs=self.epochs,
+                                       batch_size=self.batch_size,
+                                       shuffle=True,
+                                       validation_data=(
+                                           X_validation, X_validation),
+                                       verbose=0)
+    def transform(self, X):
+        
+        return self.method_enconder.predict(X)
